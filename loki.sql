6784aac200e00f275f966b8a95fa48780f927ccb
breno12filipe <breno.filipe@sempreceub.com>
2021-12-06 22:24:38 -0300

    alterando sql

--- loki.sql
+++ loki.sql
@@ -1,12 +1,10 @@
 CREATE DATABASE loki;
 
-USE loki
-
 CREATE TABLE Patient (
     patient_id SERIAL PRIMARY KEY NOT NULL,
     patient_name VARCHAR(100) NOT NULL,
     phone_number VARCHAR(14) NOT NULL,
-    birth_date DATETIME NOT NULL,
+    birth_date DATE NOT NULL,
     CPF NUMERIC(11) NOT NULL, 
     RG NUMERIC(7) NOT NULL,
     CEP VARCHAR(8), 
@@ -14,40 +12,43 @@ CREATE TABLE Patient (
     patient_address JSON,
     occupation VARCHAR(60),
     comorbidities VARCHAR(280),
-    log JSON NOT NULL
+    patient_log JSON NOT NULL
 )
 
 CREATE TABLE Anamnesis (
     anamnesis_id SERIAL PRIMARY KEY NOT NULL,
     attachment VARCHAR(10),
     body TEXT NOT NULL,
-    register_date DATETIME NOT NULL,
-    title VARCHAR(20) NOT NULL
+    register_date DATE NOT NULL,
+    title VARCHAR(20) NOT null,
+    anamnesis_log JSON NOT NULL
 )
 
-CREATE TABLE User (
+CREATE TABLE LokiUser (
     user_id SERIAL PRIMARY KEY NOT NULL,
     email VARCHAR(320) NOT NULL,
-    password VARCHAR(50) NOT NULL,
+    user_password VARCHAR(50) NOT NULL,
     /* role can be: root, admin, ordinary*/
-    user_role VARCHAR(8)
-    log JSON NOT NULL
+    user_role VARCHAR(8),
+    user_log JSON NOT NULL
 )
 
 CREATE TABLE Exam (
-    exam_id SERIAL PRIMARY KEY NOT NULL
-    exam_date DATETIME NOT NULL,
+    exam_id SERIAL PRIMARY KEY NOT null,
+    exam_date DATE NOT NULL,
     title VARCHAR(20) NOT NULL,
     /* exam type can be: simple and complementary*/
     exam_type VARCHAR(13) NOT NULL,
     attachment VARCHAR(10),
-    body TEXT NOT NULL
+    body TEXT NOT null,
+    exam_log JSON NOT NULL
 )
 
 CREATE TABLE bioimpedance (
     bioimpedance_id SERIAL PRIMARY KEY NOT NULL,
     attachment VARCHAR(10),
     body TEXT NOT NULL,
-    register_date DATETIME NOT NULL,
-    title VARCHAR(20) NOT NULL
+    register_date DATE NOT NULL,
+    title VARCHAR(20) NOT null,
+    bioimpedance_log JSON not NULL
 )
