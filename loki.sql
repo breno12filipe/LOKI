df4d036dc58a3d9e6de3fcc26d5dde5913439f6f
breno12filipe <breno.filipe@sempreceub.com>
2021-12-06 22:31:52 -0300

    alterando as tabelas patient e lokiuser do sql

--- loki.sql
+++ loki.sql
@@ -2,11 +2,11 @@ CREATE DATABASE loki;
 
 USE loki
 
-CREATE TABLE Patient (
+CREATE TABLE patient (
     patient_id SERIAL PRIMARY KEY NOT NULL,
     patient_name VARCHAR(100) NOT NULL,
     phone_number VARCHAR(14) NOT NULL,
-    birth_date DATETIME NOT NULL,
+    birth_date DATE NOT NULL,
     CPF NUMERIC(11) NOT NULL, 
     RG NUMERIC(7) NOT NULL,
     CEP VARCHAR(8), 
@@ -14,7 +14,7 @@ CREATE TABLE Patient (
     patient_address JSON,
     occupation VARCHAR(60),
     comorbidities VARCHAR(280),
-    log JSON NOT NULL
+    patient_log JSON NOT NULL
 )
 
 CREATE TABLE Anamnesis (
@@ -25,13 +25,13 @@ CREATE TABLE Anamnesis (
     title VARCHAR(20) NOT NULL
 )
 
-CREATE TABLE User (
+CREATE TABLE lokiuser (
     user_id SERIAL PRIMARY KEY NOT NULL,
     email VARCHAR(320) NOT NULL,
-    password VARCHAR(50) NOT NULL,
+    user_password VARCHAR(50) NOT NULL,
     /* role can be: root, admin, ordinary*/
     user_role VARCHAR(8)
-    log JSON NOT NULL
+    user_log JSON NOT NULL
 )
 
 CREATE TABLE Exam (
