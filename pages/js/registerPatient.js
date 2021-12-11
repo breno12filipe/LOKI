857742179d0a4e48bf7107348f388389af4ac5d5
breno12filipe <breno.filipe@sempreceub.com>
2021-12-11 16:29:26 -0300

    implementando tela de cadastrar paciente

--- pages/js/registerPatient.js
+++ pages/js/registerPatient.js
@@ -3,7 +3,8 @@ $(document).ready(function() {
     $("#telInput").mask("(00) 0-0000-0000");
     $("#dtInput").mask("99/99/9999");
     $("#CPFInput").mask("999.999.999-99");
-    $("#RGInput").mask("99.999.999-1");
+    $("#RGInput").mask("9.999.999");
+    $("#CEPInput").mask("99999-999");
 
 
         
