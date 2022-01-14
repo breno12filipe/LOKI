dec7fa91f4b2ebadfcf40f6582ff0f9a51e79891
breno12filipe <breno.filipe@sempreceub.com>
2022-01-13 23:15:23 -0300

    Icone de x nas abas para sair e duplicacao no ver e editar bioimpedancias e anamneses

--- engine/class/exam.js
+++ engine/class/exam.js
@@ -1,10 +1,13 @@
 const pool = require('../db/_connection')
 
 class exam{
-    constructor(title, description, examText){
+    constructor(title, description, examText, registerDate, examType,  patientId){
         this.title = title;
         this.description = description;
         this.examText = examText;
+        this.registerDate = registerDate;
+        this.examType = examType
+        this.patientId = patientId
     }
 
     async createExam(){
