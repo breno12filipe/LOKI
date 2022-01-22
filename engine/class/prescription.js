cea96a62c5b02b265111df731362493561f91e69
breno12filipe <breno.filipe@sempreceub.com>
2022-01-22 01:16:51 -0300

    modelando classe de prescricoes

--- engine/class/prescription.js
+++ engine/class/prescription.js
@@ -2,106 +2,94 @@ const pool = require('../db/_connection')
 const pdf = require('html-pdf')
 
 class Exam{
-    constructor(prescriptionText, prescriptionDate, title, type, description, patientId){
+    constructor(prescriptionText, prescriptionDate, title, type, description, patientId, docPath){
         this.prescriptionText = prescriptionText;
         this.prescriptionDate = prescriptionDate;
         this.title = title;
         this.type = type
         this.description = description;
         this.patientId = patientId;
+        this.docPath = docPath
     }
 
     async createPrescription(){
-        //                      EXAMS CODE
         this.log = `{
             "creation_date": "${new Date().toDateString()}",
             "creator": "${this.userAddress}",
-            "operation": "create_exam"
+            "operation": "create_prescription"
         }`;
         
-        let createExamQuery = `
-            INSERT INTO public.exam
-            (body, exam_date, title, exam_type, exam_description, exam_log, patient_id_fk)
-            VALUES('${this.examText}', '${this.examDate}', '${this.title}', 
-                   '${this.type}', '${this.description}', '${this.log}',
-                    ${this.patientId});
+        let createPrescriptionQuery = `
+            INSERT INTO public.prescription
+            (body, register_date, title, prescription_description, 
+             prescription_type, docpath, patient_id_fk, prescription_log)
+            VALUES('${this.prescriptionText}', '${this.prescriptionDate}', 
+                   '${this.title}', '${this.description}', 
+                   '${this.type}', '${this.docPath}', ${this.patientId}, '${this.log}');
         `;
 
         let response;
         try{
-            response = await pool.query(createExamQuery);
-            return "Exam inserted successfully";
+            response = await pool.query(createPrescriptionQuery);
+            return "Prescription inserted successfully";
         }catch(error){
             console.log(error)
             return error;
         }
-        //                      EXAMS CODE
     }
 
     async listPrescription(patientId){
-        //                      EXAMS CODE
         let response;
         try {
-            response = await pool.query(`SELECT * FROM public.exam 
+            response = await pool.query(`SELECT * FROM public.prescription 
                                          WHERE patient_id_fk = '${patientId}'`);
             return response.rows;
         }catch (error){
-            return "Error while listing exams";
+            return "Error while listing prescriptions";
         }
-        //                      EXAMS CODE
     }
 
-    async getPrescriptionByID(examId){
-        //                      EXAMS CODE
+    async getPrescriptionByID(prescriptionId){
         let response;
         try {
-            response = await pool.query(`SELECT * FROM public.exam WHERE exam_id = ${examId}`);
+            response = await pool.query(`SELECT * FROM public.prescription WHERE prescription_id = ${prescriptionId}`);
             return response.rows;
         }catch (error){
-            return "Error while returning Exam";
+            return "Error while returning Prescription";
         }
-        //                      EXAMS CODE
     }
 
-    async updatePrescription(examId){
-        //                      EXAMS CODE
+    async updatePrescription(prescriptionId){
         this.log = `{
             "creation_date": "${new Date().toDateString()}",
             "creator": "${this.userAddress}",
-            "operation": "update_exam"
+            "operation": "update_prescription"
         }`;
 
-        let updateExamQuery = ` UPDATE public.exam
-                                        SET body='${this.examText}', 
-                                        exam_date='${this.examDate}', 
-                                        title='${this.title}', 
-                                        exam_type='${this.type}', 
-                                        exam_description='${this.description}', 
-                                        exam_log='${this.log}', 
-                                        patient_id_fk=${this.patientId}
-                                        WHERE exam_id=${examId};
-                                    `;
-
+        let updatePrescriptionQuery = ` UPDATE public.prescription
+                                SET body='${this.prescriptionText}', register_date='${this.prescriptionDate}',
+                                title='${this.title}', prescription_description='${this.description}', 
+                                prescription_type='${this.type}', docpath='${this.docPath}', 
+                                patient_id_fk=${this.patientId}, prescription_log='${this.log}'
+                                WHERE prescription_id=${prescriptionId};
+                                `;
         let response;
         try {
-            response = await pool.query(updateExamQuery);
-            return {"responseText" : "exam updated successfully"};
+            response = await pool.query(updatePrescriptionQuery);
+            return {"responseText" : "prescription updated successfully"};
         }catch(error){
-            return {"responseText" : "Error while updating exam"};
+            return {"responseText" : "Error while updating prescription"};
         }
-        //                      EXAMS CODE
     }
 
-    async deletePrescription(examId){
-        //                      EXAMS CODE
+    async deletePrescription(prescriptionId){
         let response;
         try {
-            response = await pool.query(`DELETE FROM public.exam WHERE exam_id = ${examId};`);
-            return "Exam deleted successfully"
+            response = await pool.query(`DELETE FROM public.prescription WHERE prescriptionId = ${prescriptionId};`);
+            return "Prescription deleted successfully"
         }catch (error){
-            return "Error while deleting Exams";
+            return "Error while deleting Prescription";
         }
-        //                      EXAMS CODE
     }
 
 
